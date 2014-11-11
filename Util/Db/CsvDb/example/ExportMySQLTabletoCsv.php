8a1ae15ddfbdbc21ebbc40f0f24c8ee71c7fa11f
Sean M. Kelly <dacodestah@yahoo.com>
2014-11-11 16:02:50 -0600

    Moves example2.php under Util.Db.CsvDb where it belongs

--- Util/Db/CsvDb/example/ExportMySQLTabletoCsv.php
+++ Util/Db/CsvDb/example/ExportMySQLTabletoCsv.php
@@ -11,13 +11,13 @@ use PHPGoodies\CsvDb as CsvDb;
 use PHPGoodies\Mysql as Mysql;
 
 // 2) Load up our goodies
-require('PHPGoodies.php');
+require(dirname(__FILE__) . '/../../../../PHPGoodies.php');
 PHPGoodies::import('Lib.Db.Mysql.Mysql');
 PHPGoodies::import('Util.Db.CsvDb.CsvDb');
 
 // 3) Make a database connection
 $db = new Mysql();
-if (! $db->connect('hostname', 'username', 'password', 'databasename')) {
+if (! $db->connect('localhost', 'root', '', 'mysql')) {
 	die("error connecting to database\n");
 }
 
@@ -25,5 +25,5 @@ if (! $db->connect('hostname', 'username', 'password', 'databasename')) {
 $csvDb = new CsvDb($db);
 
 // 5) Use the CsvDb Goodie to export data from the database
-$csvDb->exportToCsv('tablename.csv', 'tablename');
+$csvDb->exportToCsv(sys_get_temp_dir() . '/user.csv', 'user');
 

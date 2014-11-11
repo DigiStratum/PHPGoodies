ba4961b42fc8afcf25fe6ee8d669f59370fd79ce
Sean M. Kelly <dacodestah@yahoo.com>
2014-11-11 16:07:17 -0600

    Moves example3 to be under Lib.Db.Mysql.exampe where it belongs

--- Lib/Db/Mysql/example/TableSchemaInfoExample.php
+++ Lib/Db/Mysql/example/TableSchemaInfoExample.php
@@ -10,16 +10,16 @@ use PHPGoodies\PHPGoodies as PHPGoodies;
 use PHPGoodies\Mysql as Mysql;
 
 // 2) Load up our goodies
-require('PHPGoodies.php');
+require(realpath(dirname(__FILE__) . '/../../../../PHPGoodies.php'));
 PHPGoodies::import('Lib.Db.Mysql.Mysql');
 
 // 3) Make a database connection
 $db = new Mysql();
-if (! $db->connect('hostname', 'username', 'password', 'databasename')) {
+if (! $db->connect('localhost', 'root', '', 'mysql')) {
 	die("error connecting to database\n");
 }
 
 // 4) Use the Mysql Goodie to print schema info for the specified database table
-$info = $db->schemaInfo('tablename');
+$info = $db->schemaInfo('user');
 print_r($info);
 

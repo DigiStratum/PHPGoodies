0ee5211afe9b6682d8e13e9e21058a8ca796bf2d
Sean M. Kelly <dacodestah@yahoo.com>
2014-11-11 19:46:58 -0600

    Adds an example for the new Lib.File.Ini class

---
+++ Lib/File/Ini/example/ReadIniExample.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPGoodies CsvTokenizeExample.php
+ *
+ * @author Sean M. Kelly <smk@smkelly.com>
+ */
+
+// 1) Adapt the name-spaced goodies to the global namespace
+use PHPGoodies\PHPGoodies as PHPGoodies;
+
+// 2) Load up our goodies
+require(realpath(dirname(__FILE__) . '/../../../../PHPGoodies.php'));
+$ini = PHPGoodies::instantiate('Lib.File.Ini.Ini', dirname(__FILE__) . '/example.ini');
+$ini->load();
+foreach ($ini->getSections() as $section) {
+	print "Section: {$section}\n";
+	print_r($ini->getSettings($section));
+}
+

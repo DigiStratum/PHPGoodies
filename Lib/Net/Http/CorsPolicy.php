638473e8e35763100f5d3442a579176c8a17ec92
Sean M. Kelly <dacodestah@yahoo.com>
2014-11-28 14:13:05 -0800

    Adds support for pattern based methodOrigins in CorsPolicy so we can do things like '*' for the origin and have it match any real origin, etc.

--- Lib/Net/Http/CorsPolicy.php
+++ Lib/Net/Http/CorsPolicy.php
@@ -10,28 +10,37 @@
  * All methods supported by HTTP1.1, RFC2616, http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html,
  * are supported (not just those commonly used by REST interfaces).
  *
- *
  * @author Sean M. Kelly <smk@smkelly.com>
  */
 
 namespace PHPGoodies;
 
+PHPGoodies::import('Lib.Net.Http.HttpRequest');
+
 /**
- * Headers
+ * CorsPolicy
  */
 class CorsPolicy {
 
+	/**
+	 * The set of origins for each method for which CORS header(s) should be provided
+	 */
 	protected $methodOrigins;
 
+	/**
+	 * Constructor
+	 *
+	 * Prepares the set of method origins with all the methods and no origins
+	 */
 	public function __construct() {
 		$this->methodOrigins = array(
-			'DELETE' => null,
-			'GET' => null,
-			'HEAD' => null,
-			'OPTIONS' => null,
-			'POST' => null,
-			'PUT' => null,
-			'TRACE' => null
+			HttpRequest::HTTP_DELETE => null,
+			HttpRequest::HTTP_GET => null,
+			HttpRequest::HTTP_HEAD => null,
+			HttpRequest::HTTP_OPTIONS => null,
+			HttpRequest::HTTP_POST => null,
+			HttpRequest::HTTP_PUT => null,
+			HttpRequest::HTTP_TRACE => null
 		);
 	}
 
@@ -60,16 +69,18 @@ class CorsPolicy {
 	/**
 	 * Add an origin to the specified method
 	 *
-	 * @param string $origin protocol://hostname:port
 	 * @param string $method The method we want to associate the origin with
+	 * @param string $origin protocol://hostname:port
 	 *
 	 * @return object $this for chaining support...
 	 */
-	public function addMethodOrigin($origin, $method) {
+	public function addMethodOrigin($method, $origin) {
+
+		$this->requireSupportedMethod($method);
 
 		// If the requested method doesn't already have any origins
-		if (! is_array($this->methodOrigins[$method])) {
-			$this->methodOrigins[$method] = array();
+		if (! is_array($this->methodOrigins[strtoupper($method)])) {
+			$this->methodOrigins[strtoupper($method)] = array();
 		}
 
 		// No effort is made to de-duplicate
@@ -78,12 +89,67 @@ class CorsPolicy {
 		return $this;
 	}
 
-	public function checkOrigin($origin, $method) {
+	/**
+	 * Check if the method has the specified origin added to it
+	 *
+	 * @param string $method The method we want to check
+	 * @param string $origin protocol://hostname:port
+	 *
+	 * @return boolean true if the method has the specified origin, else false
+	 */
+	public function doesMethodHaveOrigin($method, $origin) {
+		return is_null($tihs->getMatchingMethodOrigin($method, $origin)) ? false : true;	
 	}
 
+	/**
+	 * Get the matching methodOrigin for the requested method/origin (if any)
+	 *
+	 * The catch here is that we are suposed to support wildcards; so we will convert all the
+	 * origins that we have stored for this method into regex matching patterns, and try to
+	 * match them each in turn with the origin supplied.
+	 *
+	 * What we will return is the methodOrigin that we have who matches the requested origin
+	 * (the first one that we hit in the even that there are multiple potential matches) so
+	 * if, for example, we have a methodOrigin of '*' in the policy and the requested origin
+	 * is like 'yoursite.com', '*' will be returned because that is what matched.
+	 *
+	 * @param string $method The method we want to check
+	 * @param string $origin protocol://hostname:port
+	 *
+	 * @return string The methodOrigin that matched, or null if none
+	 */
+	public function getMatchingMethodOrigin($method, $origin) {
+		$this->requireSupportedMethod($method);
+		if (! is_array($this->methodOrigins)) return null;
+		foreach ($this->methodOrigins as $methodOrigin) {
+			$pattern = str_replace('*', '(.*?)', $methodOrigin);
+			if (preg_match("/{$pattern}/", $origin)) return $methodOrigin;
+		}
+		return null;
+	}
 
-	protected function isMethodSupported($method) {
-		return isset($this->methodOrigins[$method]);
+	/**
+	 * Check whether the specified method is supported
+	 *
+	 * @param string $method The method we want to check (case insinsitive)
+	 *
+	 * @return boolean true if the method is supported, else false
+	 */
+	public function isMethodSupported($method) {
+		return (in_array(strtoupper($method), array_keys($this->methodOrigins)));
+	}
+
+	/**
+	 * Caller forces specified method to be supported or throw exception (boilerplate reduction)
+	 *
+	 * @param string $method The method we want to check
+	 */
+	protected function requireSupportedMethod($method) {
+
+		// If the requested method is unsupported...
+		if (! $this->isMethodSupported($method)) {
+			throw new \Exception("Attempt to set CORS Policy on an unsupported method ({$method})");
+		}
 	}
 }
 

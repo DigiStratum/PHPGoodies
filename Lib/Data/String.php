054a4325d4de46b50fd90e605d26dbe54babcece
Sean M. Kelly <dacodestah@yahoo.com>
2014-11-29 13:52:04 -0800

    Adds a baseline String data class; many more methods to come; makes it usable in Collections, and brings sanity to the pile of string manipulation functions that PHP provides natively by objectifying the interface ina Java/JavaScript style (though with a custom API)

---
+++ Lib/Data/String.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * PHPGoodies:String - OOP String
+ *
+ * @todo Bring in code from the JavaPHP project (but dispense with the Java interface in favor of
+ * something more sensible/streamlined.
+ *
+ * @author Sean M. Kelly <smk@smkelly.com>
+ */
+
+namespace PHPGoodies;
+
+/**
+ * String
+ */
+class String {
+
+	/**
+	 * The string value we'll be working with
+	 */
+	protected $str;
+
+	/**
+	 * Constructor
+	 *
+	 * @param mixed $str Primitive string value OR another String object to initialize with
+	 */
+	public function __construct($str) {
+		$this->str = $this->mixedVal($str);
+	}
+
+	/**
+	 * Get the primitive string value back out easily
+	 *
+	 * @return string The value of our string
+	 */
+	public function get() {
+		return $this->str;
+	}
+
+	/**
+	 * Get the string's character length
+	 *
+	 * @todo Determine and document behavior for multibyte character sets
+	 *
+	 * @return integer length of the string in chars
+	 */
+	public function len() {
+		return strlen($this->str);
+	}
+
+	/**
+	 * Is the supplied string equal to us?
+	 *
+	 * @param mixed $str Primitive string value OR another String object to use
+	 *
+	 * @return boolean true if the string is equl to us, else false
+	 */
+	public function equals($str) {
+		return ($this->str ==  $this->mixedVal($str));
+	}
+
+	/**
+	 * Get the value of either a primitive or OOP String
+	 *
+	 * With enforcement!
+	 *
+	 * @param mixed $str Primitive string value OR another String object to use
+	 *
+	 * @return string The value of the string either way
+	 */
+	protected function mixedVal($str) {
+		return ($this->stringType($str) == 'string') ? $str : $str->get();
+	}
+
+	/**
+	 * Is the argument a string (primitive) or String (object)?
+	 *
+	 * @param mixed $str Primitive string value OR another String object to use
+	 *
+	 * @return string Either 'string' or 'String', depending
+	 */
+	protected function stringType(&$str) {
+		switch (gettype($str)) {
+			case 'string': return 'string';
+			case 'object': if (is_a($str, __CLASS__)) return 'String';
+			default: throw new \Exception("Attempted to use a String with something other than a string ('" . gettype($str) . "'");
+		}
+	}
+}
+

225cca4094f2991f6b9d3b3e3df8852939a948e7
Sean M. Kelly <dacodestah@yahoo.com>
2015-05-02 16:58:11 -0700

    Rethinking the approach since the current one will not work for nested iterated sections... more to come!

--- Lib/Template/Mustache.php
+++ Lib/Template/Mustache.php
@@ -9,6 +9,13 @@
  * Mustache templating out there, however one look at the source code made it clear that it was too
  * complicated. Here I attempt to get the same results with a simpler solution...
  *
+ * PLAN B:
+ *
+ * Process the entire template and create a nested stack (partly stack, partly tree) to show order
+ * and containment of the tags. So if section A comes BEFORE section B, then A.next = B (and B.prev
+ * = A); if section C is INSIDE of B, then B.child = C (and C.parent = B); if section D is also
+ * inside B, then C.next = D and D.prev = C.
+ *
  * @author Sean M. Kelly <smk@smkelly.com>
  */
 
@@ -68,11 +75,43 @@ class Mustache {
 
 					// The section will be rendered if the name evaluates to true,
 					// otherwise it will be collapsed and made to disappear.
-					if ($data->$name) {
-						$renderedSection = $this->renderVariables(substr($template, $sectionInnerStart, $sectionInnerEnd - $sectionInnerStart));
-					}
-					else {
+					$value = $this->getProperty($data, $name);
+
+					// Falsey values will collapse the section into nothing
+					if ((null == $value) || (false === $value) || (0 === $value)) {
 						$renderedSection = '';
+					}
+
+					// An array of objects will be iterated over with each object's
+					// properties being merged with the normal data into a temporary
+					// data structure; we'll add a rendered copy of the section for
+					// each object in the array.
+					else if (is_array($value)) {
+						$templateSection = substr($template, $sectionInnerStart, $sectionInnerEnd - $sectionInnerStart);
+						$renderedSection = '';
+						foreach ($value as $vObj) {
+
+							// Composite properties of each of the value's objects with data (vObj wins name conflicts)
+							$mdata = PHPGoodies::instantiate('Oop.Composite', Array($vObj, $data));
+
+							// FIXME: this is wrong!
+							//
+							// We should be able to iterate an array of values where each iterated chunk of template may
+							// have its own conditinoal sections; since each of those would be dependent on which element
+							// of the array is being processed, they cannot be pre-rendered depth-first. Possible solution
+							// is to handle array iterators on open, and non-array sections on close. This means we would
+							// need to somehow pre-process the stack of tags so that we know what blocks start/end where
+							// and THEN walk through the stack, processing the sections and iterators from the outside, in...
+							//
+							//
+							// It is impossible for the section being
+							// closed to have another descendant section
+							// as we would be closing IT instea of this
+							// one; thus it is not necessary to recursively
+							// call ourselves and we can just render the
+							// variables instead...
+							$renderedSection .= $this->renderVariables($templateSection, $mdata);
+						}
 					}
 
 					// Now get the parts before and after the section and connect
@@ -97,10 +136,35 @@ class Mustache {
 		}
 	}
 
-	protected function renderVariables($templateFrag, &$data) {
-		//TODO use getNextTag() to get variable tags and replace them with their values
+	/**
+	 *
+	 */
+	protected function getProperty(&$data, $name) {
+		if (property_exists($data, $name)) {
+			return $data->$name;
+		}
+		else if (method_exists($data, $name)) {
+			return $data->$name();
+		}
+		return null;
 	}
 
+	/**
+	 *
+	 */
+	protected function renderVariables($templateFrag, &$data) {
+		// Replace all the variable tags with their respective values
+		while (($tag = $this->getNextTag($templateFrag) != null) {
+			$preSection = substr($templateFrag, 0, $tag->pos);
+			$postSection = substr($templateFrag, $tag->pos + strlen($tag->tag));
+			$templateFrag = $preSection . getProperty($data, $tag->name) . $postSection;
+		}
+		return $templateFrag;
+	}
+
+	/**
+	 *
+	 */
 	protected function getNextTag($templateFrag) {
 		// Look for any tag starts...
 		$tagPos = strpos($templateFrag, '{{');
@@ -116,8 +180,8 @@ class Mustache {
 		// Return the complete tag
 		$tag = substr($template, $tagPos, $endPos - $tagPos + strlen($closer));
 		return (object) array(
-			'pos' => $tagPos,
-			'tag' => $tag,
+			'pos' => $tagPos, 	// The position of the whole tag within the templateFrag
+			'tag' => $tag,		// The whole tag, including the opening/closing braces
 			'name' => substr($tag, strlen($closer), strlen($tag) - (2 * strlen($closer)))
 		);
 	}

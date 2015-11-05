4ed446fccae9fec4a402252137ad6bd51da87758
Sean M. Kelly <dacodestah@yahoo.com>
2015-11-04 23:53:29 -0800

    Incremental progress on Domain goodie's parser of publicsuffix.org's list

--- Lib/Net/Domain.php
+++ Lib/Net/Domain.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * PHPGoodies:Domain - A class for domain/host name manipulation
+ * PHPGoodies:Domain - A class for domain/host name manipulation (WORK IN PROGRESS)
  *
  * There is enough work around this topic which is specialized to the purpose of working with
  * domain names, that it makes sense to break it off of the Url class and handle separately. The
@@ -11,7 +11,7 @@
  * reference link below reveals that aside from being quite extensive, the list appears to change
  * often enough that it would make this code difficult to maintain were all that information to be
  * hard-coded. Therefore we are going to shoot for a code-generator solution that actually pulls
- * the most recent dat file and parses it into some generated PHP with data structures that are
+ * the most recent data file and parses it into some generated PHP with data structures that are
  * well suited to looking up the top level domains and check-for and regenerate this file as needed.
  *
  * The initial driving force behind this implementation is to support HttpCookieCollection which
@@ -53,7 +53,7 @@ class Domain {
 	/**
 	 * Generate the DomainInfo class with the provided Resource information
 	 *
-	 * @param object $resourceInfo An object with nam and path properties for code generation
+	 * @param object $resourceInfo An object with name and path properties for code generation
 	 */
 	protected function generateDomainInfo($resourceInfo) {
 
@@ -70,10 +70,16 @@ class Domain {
 		$domainTree = array();
 		$line = strtok($httpResponse->body, "\n");
 		$lineNum = 0;
+		$domainSet = $ruleSet = array();
+		$ruleSet['+'] = array();
+		$ruleSet['-'] = array();
 		while ($line !== false) {
 			$line = trim($line);
 			if (strlen($line)) {
+
+				// Build a tree
 				if (strpos($line, '//') === false) {
+/*
 					$lineNum++;
 					$parts = array_reverse(explode('.', $line));
 
@@ -91,37 +97,100 @@ class Domain {
 
 						$treeBranch =& $treeBranch[$part];
 					}
+ */
+
+
+					if ($line{0} == '*') {
+						// Add to the positive rule set
+						$ruleSet['+'][] = $line;
+					}
+					else if ($line{0} == '!') {
+						// Add to the negative rule set
+						$ruleSet['-'][] = $line;
+					}
+					else {
+
+						// Add to a collection based on the string length
+						$key = $line{0} . strlen($line);
+						if (! isset($domainSet[$key])) {
+							$domainSet[$key] = array();
+						}
+						$domainSet[$key][] = $line;
+						}
+					}
+
+					// TODO: special accommodation needed for "*.tld" type patterns; what does it mean??
 				}
+				$line = strtok("\n");
 			}
-			$line = strtok("\n");
-		}
 
-		// FIXME - maybe instead we should compare one character at a time with a pile of
-		// single-char comparators until an entire string matches with early false returns
-		// whenever there is no character match...
+			// Sort the collection alphabetically
+			$keys = array_keys($domainSet);
+			foreach ($keys as $key) {
+				print "Got key: [{$key}] " . count($domainSet[$key]) . "\n";
+				sort($domainSet[$key]);
+			}
 
-		// 3) Generate an object class that makes it fast/easy
-		// to tell if a domain is in the domain tree
-		$code = '<?php' . "\n" . <<<END
-class DomainInfo {
+			print_r($ruleSet);
+return;
 
-	public static function checkDomain(\$domain) {
+			// FIXME - maybe instead we should compare one character at a time with a pile of
+			// single-char comparators until an entire string matches with early false returns
+			// whenever there is no character match...
 
-		\$parts = array_reverse(explode('.', \$domain));
+			// 3) Generate an object class that makes it fast/easy
+			// to tell if a domain is in the domain tree
+			$code = '<?php' . "\n" . <<<END
+	class DomainInfo {
+
+		public static function getDomainInfo(\$domain) {
+
+			\$domain = strtolower(\$domain);
 
 END;
-		// TODO - deal with UTF-8 (as punycode?) domains
-		$first = true;
-		$indent = "\t\t";
-		$depth = 0;
-		foreach ($domainTree as $branch => $data) {
-			$if = $first ? 'if' : 'else if';
-			$first = false;
-			
-			$code .= "{$indent}{$if} (\$parts[{$depth}] == '{$branch}') {\n";
-			$code .= "// functional code goes here\n";
-			$code .= "{$indent}}\n";
+
+			// TODO - deal with UTF-8 (as punycode?) domains
+			$first = true;
+			$indent = "\t\t";
+			$depth = 0;
+	/*
+			// Decompose the tree
+			foreach ($domainTree as $branch => $data) {
+				$if = $first ? 'if' : 'else if';
+				$first = false;
+				
+				$code .= "{$indent}{$if} (\$parts[{$depth}] == '{$branch}') {\n";
+				$code .= "// functional code goes here\n";
+				$code .= "{$indent}}\n";
+			}
+	 */
+
+			// Decompose the collection
+
+			$prefix = '';
+			$prefixSet = array();
+			foreach ($domainSet as $tld) {
+
+				// If we're in a set...
+				if (count($prefixSet) > 0) {
+
+					// And the first character of this tld matches our prefix...
+					if ($tld{0} == $first) {
+
+					// And the second character of this tld matches our prefix...
+					if ($tld{1} == $second) {
+
+						// Add this tld to the prefix set
+						$prefixSet[] = $tld;
+					}
+				}
+			}
+			else {
+				//$prefix = substr($
+				// FIXME
+			}
 		}
+
 		$code .= <<<END
 		return false;
 	}

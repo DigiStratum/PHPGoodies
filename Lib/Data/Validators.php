0961006d838e1eb5705371567f55bfc814191c8f
Sean M. Kelly <dacodestah@yahoo.com>
2015-11-06 01:17:25 -0800

    Adds validator(s) for checking directories

--- Lib/Data/Validators.php
+++ Lib/Data/Validators.php
@@ -12,7 +12,9 @@ namespace PHPGoodies;
  */
 abstract class Validators {
 
+	// -----------------------------------------------------------------------------------------
 	// STRINGS
+	// -----------------------------------------------------------------------------------------
 
 	/**
 	 * Validate that the argument is a non-empty string
@@ -35,6 +37,34 @@ abstract class Validators {
 	public static function isNonEmptyString($a) {
 		if (! self::isString($a)) return false;
 		return strlen($a) > 0;
+	}
+
+	// -----------------------------------------------------------------------------------------
+	// FILE SYSTEM
+	// -----------------------------------------------------------------------------------------
+
+	/**
+	 * Validate that the argument is a directory
+	 *
+	 * @param mixed $a Whatever we want to check with this validator
+	 *
+	 * @return boolean true if the validator is satisfied with the argument, else false
+	 */
+	public static function isDirectory($a) {
+		if (! self::isNonEmptyString($a)) return false;
+		return is_dir($a);
+	}
+
+	/**
+	 * Validate that the argument is a writable directory
+	 *
+	 * @param mixed $a Whatever we want to check with this validator
+	 *
+	 * @return boolean true if the validator is satisfied with the argument, else false
+	 */
+	public static function isWritableDirectory($a) {
+		if (! self::isDirectory($a)) return false;
+		return is_writable($a);
 	}
 }
 

00c88f20d320ddb1ed392b5c021b7a63a6865c5a
Sean M. Kelly <dacodestah@yahoo.com>
2015-12-21 21:52:00 -0800

    Adds baseline unit test coverage for mustache template renderer

---
+++ Lib/Template/test/MustacheTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * PHPGoodies:Mustache class test cases
+ *
+ * @author Sean M. Kelly <smk@smkelly.com>
+ */
+
+namespace PHPGoodies;
+
+require(realpath(dirname(__FILE__) . '/../../../PHPGoodies.php'));
+
+class MustacheTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * Our class under test
+	 */
+	private $class;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		PHPGoodies::import('Lib.Template.Mustache');
+	}
+
+	/**
+	 * Setup to occur ahead of each test method invocation
+	 */
+	public function setup() {
+		$this->class = PHPGoodies::instantiate('Lib.Template.Mustache');
+	}
+
+	/**
+	 * Teardown to occur after each test method invocation
+	 */
+	public function teardown() {
+		unset($this->class);
+	}
+
+	/**
+	 * Test that an empty template renders an empty result
+	 */
+	public function testRenderEmptyTemplateEmptyResult() {
+		$template = '';
+		$data = new \stdClass();
+		$res = $this->class->render($template, $data);
+		$this->assertEquals('', $res);
+	}
+}
+

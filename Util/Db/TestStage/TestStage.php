2c3e581bc94017cde79cdb18ef45744ce250eb6e
Sean Michael Kelly <keenskelly@gmail.com>
2016-09-30 15:42:11 -0700

    Adds TestStage class shell to stage DB data for test scenarios
    No implementation yet, just a place-holder to capture the idea. Leverage CsvDb class to move the data into the test DB, but also use this to create a DB/table, check for parial existing scenarios, roll them back, reset them, etc.

---
+++ Util/Db/TestStage/TestStage.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *
+ * PHPGoodies:TestStage - A class to help stage a databse for test scenarios
+ *
+ * @author Sean M. Kelly <smk@smkelly.com>
+ */
+
+/**
+ *
+ */
+class TestStage {
+}
+

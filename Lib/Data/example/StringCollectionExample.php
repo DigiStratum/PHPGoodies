295ab83b4c3c62474a2eec749ca23831703953a1
Sean Michael Kelly <keenskelly@gmail.com>
2016-10-02 07:59:21 -0700

    Refactors Collection Example to use GString instead of String

--- Lib/Data/example/StringCollectionExample.php
+++ Lib/Data/example/StringCollectionExample.php
@@ -1,24 +1,24 @@
 <?php
 /**
- * PHPGoodies StringCollectionExample.php
+ * PHPGoodies GStringCollectionExample.php
  *
  * @author Sean M. Kelly <smk@smkelly.com>
  */
 
 // 1) Adapt the name-spaced goodies to the global namespace
 use PHPGoodies\PHPGoodies as PHPGoodies;
-use PHPGoodies\String as String;
+use PHPGoodies\GString as String;
 
 // 2) Load up our goodies
 require(realpath(dirname(__FILE__) . '/../../../PHPGoodies.php'));
-PHPGoodies::import('Lib.Data.String');
+PHPGoodies::import('Lib.Data.GString');
 
-// 3) Fill up a collection with a bunch of Strings
-$collection = PHPGoodies::instantiate('Lib.Data.Collection', 'String');
-$collection->add(new String('Hello'));
-$collection->add(new String(', '));
-$collection->add(new String('World'));
-$collection->add(new String("!\n"));
+// 3) Fill up a collection with a bunch of GStrings
+$collection = PHPGoodies::instantiate('Lib.Data.Collection', 'GString');
+$collection->add(new GString('Hello'));
+$collection->add(new GString(', '));
+$collection->add(new GString('World'));
+$collection->add(new GString("!\n"));
 
 // 4) Iterate over the collection with a callback function that prints each string
 $collection->iterate(function ($str) {

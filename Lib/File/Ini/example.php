2390319fb68afb52c6b32941da7b7edc4569d134
Sean Michael Kelly <keenskelly@gmail.com>
2016-10-04 14:24:31 -0700

    Corrects references for example of Lib.File.Ini Goody

--- Lib/File/Ini/example.php
+++ Lib/File/Ini/example.php
@@ -9,8 +9,8 @@
 use PHPGoodies\PHPGoodies as PHPGoodies;
 
 // 2) Load up our goodies
-require(realpath(dirname(__FILE__) . '/../../../../PHPGoodies.php'));
-$ini = PHPGoodies::instantiate('Lib.File.Ini.Ini', dirname(__FILE__) . '/example.ini');
+require(realpath(dirname(__FILE__) . '/../../../PHPGoodies.php'));
+$ini = PHPGoodies::instantiate('Lib.File.Ini', dirname(__FILE__) . '/example.ini');
 $ini->load();
 foreach ($ini->getSections() as $section) {
 	print "Section: {$section}\n";

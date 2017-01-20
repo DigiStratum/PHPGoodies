77b62b125a5b97c15add2a8cf224362cddd3d33e
Sean Michael Kelly <keenskelly@gmail.com>
2017-01-19 17:38:16 -0800

    Adds the class shell for PHPDeps Utility goodie
    We want to examine each PHP file provided, change to its directory, and follow relative paths as well as partial paths from the script execution directory as well as each file's directory to see whatever other PHP files we can find... and recurse on each of them. This will require looking at requires and includes and doing some file finds on the filesystem - it might be easiest if we build a filesystem tree in memory in advance and then scan that from in-memory instead of repeatedly doing finds on the filesystem.

---
+++ Util/PHPDeps/implementation.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * PHPGoodies:Util_PHPDeps - A tool to discover on-disk PHP dependencies
+ *
+ * @author Sean M. Kelly <smk@smkelly.com>
+ */
+
+namespace PHPGoodies;
+
+/**
+ * PHPDeps - A tool to discover on-disk PHP dependencies
+ */
+class Util_PHPDeps {
+
+	/**
+	 * Constructor
+	 *
+	 * @param $files Array of filename strings to start our discovery
+	 */
+	public function __construct($files) {
+	}
+}
+

db4ee958bef1615288d361f67714ff0a4f69ae5c
Sean Michael Kelly <keenskelly@gmail.com>
2017-01-21 00:01:49 -0800

    Changes JSON:API Resource into an abstract class instead of interface
    This lets us put some baseline functionality into the abstract class which each of the subclasses will not need to implement (and, in fact, cannot tamper with by overriding)

--- Lib/Net/Api/Rest/JsonApi/Server/Resource/implementation.php
+++ Lib/Net/Api/Rest/JsonApi/Server/Resource/implementation.php
@@ -1,22 +1,104 @@
 <?php
 /**
- * PHPGoodies:Lib_Api_Rest_JsonApi_Resource - Class interface for Resources
+ * PHPGoodies:Lib_Api_Rest_JsonApi_Resource - Abstract class for a single resource
+ *
+ * @uses Lib_Data_Hash
+ * @uses Lib_Net_Api_Rest_JsonApi_Server_Attribute
+ * @uses Lib_Net_Api_Rest_JsonApi_Server_ResourceData
  *
  * @author Sean M. Kelly <smk@smkelly.com>
  */
 
 namespace PHPGoodies;
 
+
 /**
- * JSON:API Resource Interface
+ * JSON:API Single resource base class
+ *
+ * Each of the abstract methods is documented with certain expectations of the subclass behavior.
  */
-interface Lib_Net_Api_Rest_JsonApi_Resource {
+abstract class Lib_Net_Api_Rest_JsonApi_Resource {
+
+	/**
+	 * Our resource data
+	 */
+	private $data;
+
+	/**
+	 * Constructor
+	 *
+	 * @param $type string type of resource we are working with
+	 */
+	public function __construct($type) {
+
+		// Declare each of the attributes' names in the resource data
+		$attributes = PHPGoodies::instantiate('Lib.Data.Hash');
+		$attributeNames = $this->getAttributeNames();
+		if ((! is_array($attributeNames)) || (count($attributeNames) === 0)) {
+			throw new \Exception("No attribute names were supplied");
+		}
+		foreach ($attributeNames as $attributeName) {
+			$attributes->set(
+				$attributeName,
+				PHPGoodies::instantiate('Lib.Net.Api.Rest.JsonApi.Server.Attribute', $attributeName)
+			);
+		}
+		$this->data = PHPGoodies::instantiate('Lib.Net.Api.Rest.JsonApi.Server.ResourceData', $type, $attributes);
+	}
+
+	/**
+	 * Gets a list of attribute names
+	 */
+	abstract public function getAttributeNames();
+
+	/**
+	 * Set the current data
+	 */
+	public function setData($data);
+
+	/**
+	 * Get the current data
+	 */
+	public function getData();
+
+	/**
+	 * Returns the URI, relative to the API base URL, which will deliver this exact resource
+	 */
+	public function getUri();
+
+	/**
+	 * Accepts resource data from 
+	 */
 	public function create();
+
+	/**
+	 *
+	 */
 	public function retrieve();
+
+	/**
+	 *
+	 */
 	public function update();
+
+	/**
+	 *
+	 */
 	public function delete();
+
+	/**
+	 *
+	 */
 	public function validate();
+
+	/**
+	 *
+	 */
 	public function toJson();
+
+	/**
+	 *
+	 */
 	public function fromJson($json);
 }
 

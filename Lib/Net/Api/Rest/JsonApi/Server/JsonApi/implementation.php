8f498ee9fbf70a4a6f36cf9ccfdc1cef59ce483d
Sean Michael Kelly <keenskelly@gmail.com>
2017-01-23 23:45:21 -0800

    Adds a JSON_API server info class for response documents

---
+++ Lib/Net/Api/Rest/JsonApi/Server/JsonApi/implementation.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PHPGoodies:Lib_Api_Rest_JsonApi_Server_JsonApi - JSON:API Server Info Class
+ *
+ * @author Sean M. Kelly <smk@smkelly.com>
+ */
+
+namespace PHPGoodies;
+
+/**
+ * JSON:API Server Info class
+ */
+class Lib_Net_Api_Rest_JsonApi_Server_JsonApi {
+	public $version = "1.0";
+}
+

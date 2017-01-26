66df033ae1a60bbcb685031db02fe04797563e67
Sean Michael Kelly <keenskelly@gmail.com>
2017-01-25 21:04:26 -0800

    Adds ResourceIdentifier class for JsonApi response documents

---
+++ Lib/Net/Api/Rest/JsonApi/Server/ResourceIdentifier/implementation.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * PHPGoodies:Lib_Api_Rest_JsonApi_Server_ResourceIdentifier - Resource Identifier Class
+ *
+ * @author Sean M. Kelly <smk@smkelly.com>
+ */
+
+namespace PHPGoodies;
+
+/**
+ * JSON:API Resource Identifier class
+ */
+class Lib_Net_Api_Rest_JsonApi_Server_ResourceIdentifier implements \JsonSerializable {
+
+	/**
+	 * unique identifier for this resource (within the type)
+	 */
+	protected $id;
+
+	/**
+	 * type of resource
+	 */
+	protected $type;
+
+	/**
+	 * non-standard metadata for this resource; optional, may be null
+	 */
+	protected $meta;
+
+	/**
+	 * Constructor
+	 *
+	 * @param $id String unique identifier for this resource (within the type)
+	 * @param $type String type of resource
+	 * @param $meta Meta class instance; optional, default is null
+	 */
+	public function __construct(string $id, string $type, Lib_Net_Api_Rest_JsonApi_Meta $meta = null) {
+		$this->id = $id;
+		$this->type = $type;
+		$this->meta = $meta;
+	}
+
+	/**
+	 * JsonSerializable Json Serializer
+	 *
+	 * @return Associative array of properties which will be encoded as the JSON representation
+	 * of object instances of this class
+	 */
+	public function jsonSerialize() {
+		return [
+			'id' => $this->id,
+			'type' => $this->type,
+			'meta' => $this->meta
+		];
+	}
+}
+

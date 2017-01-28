7bd88c9a916059cd06987d807667a0d62383f6a8
Sean Michael Kelly <keenskelly@gmail.com>
2017-01-28 15:04:59 -0800

    Refactors to use JSON serializer of super class

--- Lib/Net/Api/Rest/JsonApi/Server/Errors/implementation.php
+++ Lib/Net/Api/Rest/JsonApi/Server/Errors/implementation.php
@@ -12,29 +12,13 @@ PHPGoodies::import('Lib.Data.Collection');
 /**
  * JSON:API Error Collection for response documents
  */
-class Lib_Net_Api_Rest_JsonApi_Server_Errors extends Lib_Data_Collection implements \JsonSerializable {
+class Lib_Net_Api_Rest_JsonApi_Server_Errors extends Lib_Data_Collection {
 
 	/**
 	 * Constructor
 	 */
 	public function __construct() {
 		parent::__construct('Lib_Net_Api_Rest_JsonApi_Server_Error');
-	}
-	
-	/**
-	 * JsonSerializable Json Serializer
-	 *
-	 * @return Associative array of properties which will be encoded as the JSON representation
-	 * of object instances of this class
-	 */
-	public function jsonSerialize() {
-		$errors = array();
-		$this->iterate(
-			function ($error) use (&$errors) {
-				$errors[] = $error;
-			}
-		);
-		return $errors;
 	}
 }
 

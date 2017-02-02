6b4223a8197c98e97f097c2c1e6df13848b55ce2
Sean Michael Kelly <keenskelly@gmail.com>
2017-02-01 23:59:59 -0800

    Adds Oop_Type Goodie for checking data types

---
+++ Oop/Type/implementation.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * PHPGoodies:Oop_Type - An assortment of utility methods to help us with data types
+ *
+ * @author Sean M. Kelly <smk@smkelly.com>
+ */
+
+namespace PHPGoodies;
+
+
+
+/**
+ * An assortment of utility methods to help us with data types
+ */ 
+abstract class Oop_Type {
+
+	/**
+	 * Get the 'type' for the supplied object
+	 *
+	 * Our types expand on the PHP primitives by combining objects with their class names if
+	 * they are anything more specific than 'StdClass'
+	 *
+	 * @param mixed $data Some data/callable entity that we want a type for
+	 * @param boolean $ignoreNamespace Defaults to true to remove the namespace from class names
+	 *
+	 * @return string the type of the object that we will use internally
+	 */
+	public static function getType(&$data, $ignoreNamespace = true) {
+
+		// Get the native type; strtolower makes 'NULL' into 'null'
+		$type = strtolower(gettype($data));
+
+		if ('object' !== $type) return $type;
+
+		// Nope, must be a regular object class
+		$class = get_class($data);
+
+		// Functions are class "Closure"
+		if ('Closure' == $class) return 'function';
+
+		// Is it a standard class (unnamed)? 
+		if ('stdClass' == $class) return 'object';
+
+		// Anything else is a normal class; if it is namespaced...
+		if ($ignoreNamespace && strpos($class, '\\')) {
+			// Remove the namespace and just get the class name
+			$class = substr($class, strrpos($class, '\\') + 1);
+		}
+
+		return "class:{$class}";
+	}
+
+	/**
+	 * Is the supplied data of the specified type?
+	 *
+	 * @param mixed $data Any PHP data we want to check the type of
+	 * @param string $type Descriptor of a valid type including a class name
+	 * @param boolean $ignoreNamespace Defaults to true to remove the namespace from class names
+	 *
+	 * @return boolean true if the data's type matches the specified type, else false
+	 */
+	public static function isType(&$data, $type, $ignoreNamespace = true) {
+		return (static::getType($data, $ignoreNamespace) == $type);
+	}
+
+	/**
+	 * Throws an exception if the supplied data does NOT match the specified type
+	 *
+	 * @param mixed $data Any PHP data we want to check the type of
+	 * @param string $type Descriptor of a valid type including a class name
+	 * @param boolean $ignoreNamespace Defaults to true to remove the namespace from class names
+	 *
+	 * @throws Oop_Exception_TypeMismatch if the type does not match
+	 */
+	public static function requireType(&$data, $type, $ignoreNamespace = true) {
+		if (static::isType($data, $type, $ignoreNamespace)) return;
+		throw new Oop_Exception_TypeMismatch("Expected '$type', but got '" . static::getType($data, $ignoreNamespace) . "'");
+	}
+}
+
---
+++ Oop/Type/test.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * PHPGoodies:Type class test cases
+ *
+ * @author Sean M. Kelly <smk@smkelly.com>
+ */
+
+namespace PHPGoodies;
+
+require_once(realpath(dirname(__FILE__) . '/../../PHPGoodies.php'));
+
+PHPGoodies::import('Oop.Exception.TypeMismatch');
+
+class SimpleTestClass {}
+
+class TypeTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		PHPGoodies::import('Oop.Type');
+	}
+
+	/**
+	 * Setup to occur ahead of each test method invocation
+	 */
+	public function setup() {
+	}
+
+	/**
+	 * Teardown to occur after each test method invocation
+	 */
+	public function teardown() {
+	}
+
+	/**
+	 * Test that getType strtolowers its results
+	 */
+	public function testThatNullsAreLowercase() {
+		$data = NULL;
+		$type = Oop_Type::getType($data);
+		$this->assertEquals($type, 'null');
+	}
+
+	/**
+	 * Test that 'Closure's are returned as 'function's
+	 */
+	public function testThatClosuresAreFunctions() {
+		$data = function () { print('Hello, World!'); };
+		$type = Oop_Type::getType($data);
+		$this->assertEquals($type, 'function');
+	}
+
+	/**
+	 * Test that 'stdClass'es are returned as 'object's
+	 */
+	public function testThatStdClassesAreObjects() {
+		$data = new \StdClass();
+		$type = Oop_Type::getType($data);
+		$this->assertEquals($type, 'object');
+	}
+
+	/**
+	 * Test that any other object class is returned as 'class:classname'
+	 */
+	public function testThatOtherClassesAreClasses() {
+		$data = new SimpleTestClass();
+		$type = Oop_Type::getType($data);
+		$this->assertEquals($type, 'class:SimpleTestClass');
+	}
+
+	/**
+	 * Test that namespace is properly ignored
+	 */
+	public function testThatNamespaceIsProperlyIgnored() {
+		$data = new SimpleTestClass();
+		$type = Oop_Type::getType($data, false);
+		$this->assertEquals($type, 'class:PHPGoodies\\SimpleTestClass');
+		$type = Oop_Type::getType($data);
+		$this->assertEquals($type, 'class:SimpleTestClass');
+	}
+
+	/**
+	 * Test that all other data types are preserved
+	 */
+	public function testThatTypesArePreserved() {
+		$data = 3;
+		$type = Oop_Type::getType($data);
+		$this->assertEquals($type, 'integer');
+		$data = 3.14159;
+		$type = Oop_Type::getType($data);
+		$this->assertEquals($type, 'double');
+		$data = 'Hello, World';
+		$type = Oop_Type::getType($data);
+		$this->assertEquals($type, 'string');
+		$data = true;
+		$type = Oop_Type::getType($data);
+		$this->assertEquals($type, 'boolean');
+		$data = Array();
+		$type = Oop_Type::getType($data);
+		$this->assertEquals($type, 'array');
+		$data = fopen(__FILE__, 'r');
+		$type = Oop_Type::getType($data);
+		fclose($data);
+		$this->assertEquals($type, 'resource');
+	}
+
+	/**
+	 * Test that isType matches what we expect it to
+	 */
+	public function testThatIsTypeMatchesExpected() {
+		$data = NULL;
+		$this->assertTrue(Oop_Type::isType($data, 'null'));
+		$data = 3;
+		$this->assertTrue(Oop_Type::isType($data, 'integer'));
+		$data = 3.14159;
+		$this->assertTrue(Oop_Type::isType($data, 'double'));
+		$data = 'Hello, World!';
+		$this->assertTrue(Oop_Type::isType($data, 'string'));
+		$data = true;
+		$this->assertTrue(Oop_Type::isType($data, 'boolean'));
+		$data = Array();
+		$this->assertTrue(Oop_Type::isType($data, 'array'));
+		$data = new \StdClass();
+		$this->assertTrue(Oop_Type::isType($data, 'object'));
+		$data = new SimpleTestClass();
+		$this->assertTrue(Oop_Type::isType($data, 'class:SimpleTestClass'));
+		$data = fopen(__FILE__, 'r');
+		$res = Oop_Type::isType($data, 'resource');
+		fclose($data);
+		$this->assertTrue($res);
+	}
+
+	/**
+	 * Test that isType rejects the unexpected
+	 */
+	public function testThatIsTypeRejectsTheUnexpected() {
+		$data = NULL;
+		$this->assertFalse(Oop_Type::isType($data, 'french fries!'));
+	}
+
+	/**
+	 * Test that isType carries through the namespace ignore flag
+	 */
+	public function testThatIsTypeCarriesThroughNamespaceIgnorance() {
+		$data = new SimpleTestClass();
+		$this->assertTrue(Oop_Type::isType($data, 'class:PHPGoodies\\SimpleTestClass', false));
+	}
+
+	/**
+	 * Test that requireType throws an exception on mismatch
+	 *
+	 * @expectedException PHPGoodies\Oop_Exception_TypeMismatch
+	 */
+	public function testThatRequireTypeExceptsOnMismatch() {
+		$data = new SimpleTestClass();
+		Oop_Type::requireType($data, 'french fries!');
+	}
+
+	/**
+	 * Test that requireType does NOT throw an exception on match
+	 */
+	public function testThatRequireTypeAcceptsOnMatch() {
+		$data = 3;
+		Oop_Type::requireType($data, 'integer');
+	}
+}

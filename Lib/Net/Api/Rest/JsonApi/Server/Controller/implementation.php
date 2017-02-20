7e615652d7c7fcb2b354d580d9b575147c8b3ec4
Sean Michael Kelly <keenskelly@gmail.com>
2017-02-19 17:42:10 -0800

    Incremental WIP for JsonApi Server's Controller layer

--- Lib/Net/Api/Rest/JsonApi/Server/Controller/implementation.php
+++ Lib/Net/Api/Rest/JsonApi/Server/Controller/implementation.php
@@ -3,6 +3,8 @@
  * PHPGoodies:Lib_Api_Rest_JsonApi_Controller - JSON:API Endpoint Controller
  *
  * @uses Oop_Type
+ * @uses Lib_Net_Api_Rest_JsonApi_Server_UriPattern
+ * @uses Lib_Net_Api_Rest_JsonApi_Server_Service
  *
  * @author Sean M. Kelly <smk@smkelly.com>
  */
@@ -34,57 +36,124 @@ abstract class Lib_Net_Api_Rest_JsonApi_Controller {
 	 * @param object $service Service implementation which we can count on
 	 */
 	public function __controller($uriPattern, $service) {
-		$this->uriPattern = Oop_Type::requireType($uriPattern, 'class:Lib_Net_Api_Rest_JsonApi_Server_Uri_Pattern');
+		$this->uriPattern = Oop_Type::requireType($uriPattern, 'class:Lib_Net_Api_Rest_JsonApi_Server_UriPattern');
 		$this->service = Oop_Type::requireType($service, 'class:Lib_Net_Api_Rest_JsonApi_Server_Service');
 	}
 
 	/**
-	 * Return the UriPattern which attaches us
+	 * Handle an HTTP OPTIONS request
 	 *
-	 * @return object Uri_Pattern class instance
+	 * @param string $uri The URI of the request we want to match against our UriPattern
+	 *
+	 * @return object HTTPResponse instance
 	 */
-	abstract static public function getUriPattern();
+	public function doOptions($uri) {
+	}
 
 	/**
-	 * Handle an HTTP GET request
+	 * Checks whether the UriPattern which attaches us matches the supplied URI
+	 *
+	 * @param string $uri Portion of a URL that we want to know whether is a match for us
+	 *
+	 * @return boolean true if the supplied URI matches us, else false
 	 */
-	public function doGet() {
-		$this->service->retrieve();
-	}
+	abstract static public function matchesUri($uri);
 
 	/**
 	 * Handle an HTTP POST request
+	 *
+	 * @param string $uri The URI of the request we want to match against our UriPattern
+	 *
+	 * @return object HTTPResponse instance
 	 */
-	public function doPost() {
-		$this->service->create();
+	public function doPost($uri) {
+		$this->serviceCall('create', $uri);
 	}
 
 	/**
-	 * Handle an HTTP PUT request
+	 * Handle an HTTP GET request
+	 *
+	 * @param string $uri The URI of the request we want to match against our UriPattern
+	 *
+	 * @return object HTTPResponse instance
 	 */
-	public function doPut() {
-		$this->service->replace();
+	public function doGet($uri) {
+		$this->serviceCall('retrieve', $uri);
 	}
 
 	/**
 	 * Handle an HTTP PATCH request
+	 *
+	 * @param string $uri The URI of the request we want to match against our UriPattern
+	 *
+	 * @return object HTTPResponse instance
 	 */
-	public function doPatch() {
-		$this->service->modify();
+	public function doPatch($uri) {
+		$this->serviceCall('update', $uri);
+	}
+
+	/**
+	 * Handle an HTTP DELETE request
+	 *
+	 * @param string $uri The URI of the request we want to match against our UriPattern
+	 *
+	 * @return object HTTPResponse instance
+	 */
+	public function doDelete($uri) {
+		$this->serviceCall('delete', $uri);
+	}
+
+	/**
+	 * Handle an HTTP PUT request
+	 *
+	 * @param string $uri The URI of the request we want to match against our UriPattern
+	 *
+	 * @return object HTTPResponse instance
+	 */
+	public function doPut($uri) {
+		$this->serviceCall('replace', $uri);
 	}
 
 	/**
 	 * Handle an HTTP HEAD request
+	 *
+	 * @param string $uri The URI of the request we want to match against our UriPattern
+	 *
+	 * @return object HTTPResponse instance
 	 */
-	public function doHead() {
-		$this->service->peek();
+	public function doHead($uri) {
+		$this->serviceCall('getMetadata', $uri);
 	}
 
 	/**
-	 * Handle an HTTP OPTIONS request
+	 * Make dispath any of the above handlers' service calls
+	 *
+	 * @param string $methodName Name of the service method we want to invoke
+	 * @param string $uri The URI of the request we want to match against our UriPattern
+	 *
+	 * @return object HTTPResponse instance
+	 *
+	 * @throws InvalidRequest
 	 */
-	public function doOptions() {
-		$this->service->check();
+	protected function serviceCall($methodName, $uri) {
+
+		// Formulate an HTTP response, for better or worse!
+		$response = PHPGoodies::instantiate('Lib.Net.Http.Response');
+
+		try {
+			$vars = $this->uriPattern->getUriVariables($uri);
+			if (is_null($args)) {
+				throw new \Exception('400 BAD REQUEST');
+			}
+		}
+
+		// Map JsonApi.Server.Exceptions to HTTP RESPONSES
+		catch (Lib_Net_Api_Rest_JsonApi_Server_Exception_NoAuthentication $e) {
+		}
+		catch (Lib_Net_Api_Rest_JsonApi_Server_Exception, \Exception $e) {
+		}
+
+		return $this->service->{$methodName}($vars);
 	}
 }
 

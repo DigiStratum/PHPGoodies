7855e106c45d7ca24964fa2328a85397cf19c0da
Sean Michael Kelly <keenskelly@gmail.com>
2017-02-21 23:59:47 -0800

    Adds a bunch of mapped exceptions for HttpResponse
    And makes HttpResponse's getDescription method statically publicly available to improve its usefulness

--- Lib/Net/Http/Response/MappedException/Gone/implementation.php
+++ Lib/Net/Http/Response/MappedException/Gone/implementation.php
@@ -14,16 +14,16 @@ PHPGoodies::import('Lib.Net.Http.Response');
 PHPGoodies::import('Lib.Net.Http.Response.MappedException');
 
 /**
- * MappedException: 400 BAD REQUEST
+ * MappedException: 410 GONE
  */
-abstract class Lib_Net_Http_Response_MappedException_BadRequest extends Lib_Net_Http_Response_MappedException {
+abstract class Lib_Net_Http_Response_MappedException_Gone extends Lib_Net_Http_Response_MappedException {
 
 	/**
 	 * Constructor - code is "fixed" for these mapped exceptions, so we don't allow code to be supplied
 	 */
 	public function __construct($message = '', $previous = null) {
 		$msg = (strlen($message)) ? " - {$message}" : '';
-		$code = Lib_Net_Http_Response::HTTP_BAD_REQUEST;
+		$code = Lib_Net_Http_Response::HTTP_GONE;
 		$desc = Lib_Net_Http_Response::getDescription($code);
 		parent::__construct("{$desc}{$m}", $code, $previous);
 	}

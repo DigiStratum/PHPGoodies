872b13c42ff828b1afe373399a603029d0f74220
Sean Michael Kelly <keenskelly@gmail.com>
2017-03-05 22:07:31 -0800

    Straightens up JSON:API Server Controller example so it minimally functions

--- Lib/Net/Api/Rest/JsonApi/Server/Controller/example.php
+++ Lib/Net/Api/Rest/JsonApi/Server/Controller/example.php
@@ -1,21 +1,22 @@
 <?php
 /**
- * PHPGoodies StringCollectionExample.php
+ * PHPGoodies JSON:API Server Controller Example
  *
  * @author Sean M. Kelly <smk@smkelly.com>
  */
 
 // 1) Adapt the name-spaced goodies to the global namespace
 use PHPGoodies\PHPGoodies as PHPGoodies;
-use PHPGoodies\Lib_Net_Api_Rest_JsonApi_Server_Controller as Controller;
-use PHPGoodies\Lib_Net_Api_Rest_JsonApi_Server_Service as Service;
+use PHPGoodies\Lib_Net_Api_Rest_JsonApi_Server_Controller as Lib_Net_Api_Rest_JsonApi_Server_Controller;
+use PHPGoodies\Lib_Net_Api_Rest_JsonApi_Server_Service as Lib_Net_Api_Rest_JsonApi_Server_Service;
 
 // 2) Load up our goodies
 require(realpath(dirname(__FILE__) . '/../../../../../../../PHPGoodies.php'));
 PHPGoodies::import('Lib.Net.Api.Rest.JsonApi.Server.Controller');
+PHPGoodies::import('Lib.Net.Api.Rest.JsonApi.Server.Service');
 
 // 3) Provide an implementation for a Service
-class MyService implements Service {
+class MyService implements Lib_Net_Api_Rest_JsonApi_Server_Service {
 	public function create($variables) {return true;}
         public function retrieve($variables) {return true;}
         public function update($variables) {return true;}
@@ -25,9 +26,10 @@ class MyService implements Service {
 }
 
 // 4) Provide an implementation for a Controller
-class MyController extends Controller {
+class MyController extends Lib_Net_Api_Rest_JsonApi_Server_Controller {
 
 	public function __construct($service) {
+		print "fired up!\n\n";
 		parent::__construct(self::getUriPattern(), $service);
 	}
 
@@ -42,11 +44,9 @@ class MyController extends Controller {
 }
 
 
-$collection = PHPGoodies::instantiate('Lib.Data.Collection', 'String');
-
 $uris = Array(
-	'/path/number/dir/string',
-	'/path/333/dir/string'
+	'/path/number/dir/string',	// NO match
+	'/path/333/dir/string'		// matches!
 );
 
 foreach ($uris as $uri) {
